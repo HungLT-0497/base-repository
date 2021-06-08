0aaebf90f018c58ffa796112b311620f4df6f12c
HungLt-0497 <hunglt@funtap.vn>
2021-06-08 13:59:22 +0700

    fcm

---
+++ src/RepositoryServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace HungLT;
+
+use Illuminate\Support\ServiceProvider;
+use HungLT\Console\RepositoryMakeCommand;
+use HungLT\Console\RepositoryInterfaceMakeCommand;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    protected $commands = [
+        RepositoryMakeCommand::class,
+        RepositoryInterfaceMakeCommand::class
+    ];
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->commands($this->commands);
+    }
+}
